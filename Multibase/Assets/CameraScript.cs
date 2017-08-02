f443bcc34ca725d7165d7ed644e52fa861c9d3ce
Unknown <theyurch@gmail.com>
2017-08-01 20:39:21 -0400

    Added stupid fixed point 3d style camera

---
+++ Multibase/Assets/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+    Quaternion lastRot;
+
+	// Use this for initialization
+	void Start () {
+        lastRot = Quaternion.identity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (target)
+        {
+            lastRot = Quaternion.Lerp(lastRot, Quaternion.LookRotation(target.position - transform.position), 0.05f);
+            transform.rotation = lastRot;
+        }
+        
+	}
+}
--- Multibase/Assets/Playermove.cs
+++ Multibase/Assets/Playermove.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 using UnityEngine.Networking;
 
+
 public class Playermove : NetworkBehaviour
 {
     GameObject body;
     Rigidbody bodyRigid;
     Transform bodyPos;
     public Vector3 moveDir;
-
+    //GameObject cubeCamera;
+    /*
+    void OnNetworkInstantiate(NetworkMessageInfo info)
+    {
+        if (isLocalPlayer)
+        { 
+            Camera.main.GetComponent<CameraScript>().target = transform;
+        }
+    }
+    */
     // Use this for initialization
     void Start () {
        /*
@@ -21,6 +31,12 @@ public class Playermove : NetworkBehaviour
         body = this.gameObject;
         bodyPos = body.GetComponent<Transform>();
         bodyRigid = body.GetComponent<Rigidbody>();
+        //cubeCamera = transform.Find("Camera").gameObject;
+
+        if (isLocalPlayer)
+        {
+            Camera.main.GetComponent<CameraScript>().target = transform;
+        }
 
     }
 	
@@ -33,13 +49,28 @@ public class Playermove : NetworkBehaviour
         
         var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
-        moveDir = new Vector3(x, 0, z);
-        
+
+        var vectFor = new Vector3(z, 0, -x);
+        vectFor = Camera.main.GetComponent<Transform>().rotation * vectFor;
+        moveDir = Vector3.Cross(vectFor, new Vector3(0, 1, 0));
+     //   moveDir.z *= z;
+     //   moveDir.x *= x;
+        // moveDir = new Vector3(x, 0, z);
+
         bodyRigid.AddForceAtPosition
-            (moveDir.normalized * 6, 
+            (moveDir.normalized * 5, 
             bodyPos.position, 
             ForceMode.Acceleration);
         
         //transform.Translate(x, 0, z);
+    }
+    void OnDrawGizmos()
+    {
+       // if ()
+       // {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(bodyPos.position, moveDir * 10);
+       // }
+
     }
 }
