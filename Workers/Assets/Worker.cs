4ceb4148a8d98c819efd576bb5bbc3f03d99c9df
Unknown <theyurch@gmail.com>
2018-05-05 15:22:59 -0400

    I straight up hate this thing

--- Workers/Assets/Worker.cs
+++ Workers/Assets/Worker.cs
@@ -7,18 +7,36 @@ public class Worker : MonoBehaviour {
 
     public Transform moveLocation;
     NavMeshAgent navAgent;
-
+    LineRenderer navLine;
+    public bool hasObject = false;
 	// Use this for initialization
 	void Start () {
         navAgent = GetComponent<NavMeshAgent>();
 
         navAgent.Warp(transform.position);
+        navLine = GetComponent<LineRenderer>();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        navAgent.SetDestination(moveLocation.position);
+        if (navAgent && moveLocation)
+        {
+
+            navAgent.SetDestination(moveLocation.position);
+            //navAgent.hasPath
+            navLine.SetPosition(0, transform.position);
+            navLine.SetPosition(1, navAgent.steeringTarget);
+            //navAgent.steeringTarget
+        
+
+        }
+        
         
 	}
+
+    void OnDrawGizmos()
+    {
+        
+    }
 }
